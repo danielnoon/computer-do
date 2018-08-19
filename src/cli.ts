d85765d855381a376cb0faf56d7214be82237a12
Daniel Noon <delpinothedragon1@hotmail.com>
2018-08-19 01:37:54 -0500

    create cli!

---
+++ src/cli.ts
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+import * as program from 'commander';
+import * as fs from 'fs';
+import * as readline from 'readline';
+
+import Interpreter from './Interpreter';
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+program
+  .version('0.1.0')
+  .usage('<source>')
+  .parse(process.argv);
+
+const file = fs.readFileSync(program.args[0], 'utf-8');
+
+const interpreter = new Interpreter();
+interpreter.run(file);
+process.exit();
